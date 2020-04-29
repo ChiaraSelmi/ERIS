81304b4371a1c78c028cc83299ee239270c8aa4d
cselmi <rm@air-di-rosanna.station>
2020-04-29 20:12:49 +0200

    2020.04.29

--- eris/strehl_ratio_calculator.py
+++ eris/strehl_ratio_calculator.py
@@ -23,9 +23,12 @@ class SR_Calculator():
         self._lambda = 1300e-9
 
     def principal_main(self, psf_ima):
+        ''' Eseguo il procedimento usando le dimenzioni totali del frame
+        e alla fine eseguo la normalizzazione sull'immagine ritaziata al PixelCut
+        '''
         psf_ima_cut, y_min, x_min = self.ima_cutter(psf_ima, self._pixelCut)
         par_cut, par_psf_ima = self.fit_2dGaussianForPsfImaCut(psf_ima_cut, y_min, x_min)
-        xx, yy, rmin, rmax, final_bgr = self.bgr_calc(psf_ima, par_psf_ima)
+        xx, yy, rmin, rmax, final_bgr = self.bgr_calc_rings(psf_ima, par_psf_ima)
 
         psf_ima_no_bgr = psf_ima - final_bgr
 
@@ -46,14 +49,18 @@ class SR_Calculator():
 #        plot(x, norm_psf_ima_no_bgr[146,280:350], label='psf_no_bgr');plot(x, norm_psf_diffraction_limited[146,280:350], label='psf_dl'); plt.xlabel('Pixel'); plt.title('SR = %f' %strehl_ratio); plt.legend()
 #
 #        plot(x, aa[146,280:350], label='psf_no_bgr');plot(x, bb[146,280:350], label='psf_dl'); plt.xlabel('Pixel'); plt.legend()
-#        
+#
 #        x= np.arange(40)
 #        plot(x, norm_psf_ima_no_bgr[20,0:40], label='psf_no_bgr');plot(x, norm_psf_diffraction_limited[20,0:40], label='psf_dl'); plt.xlabel('Pixel');plt.title('SR = %f' %strehl_ratio); plt.legend()
 
-    def principal_main_cut(self, psf_ima):
-        psf_ima_cut, y_min, x_min = self.ima_cutter(psf_ima, 50)
+    def principal_main_cut(self, psf_ima, cut):
+        ''' Passo cut=numero di pixel per ritagliare il frame iniziale
+        ed eseguo tutto il procedimento su quella dimenzione lì
+        '''
+        psf_ima_cut, y_min, x_min = self.ima_cutter(psf_ima, cut)
         par_cut = fit_2dgaussian(psf_ima_cut)._parameters
-        xx, yy, rmin, rmax, final_bgr = self.bgr_calc(psf_ima_cut, par_cut)
+        xx, yy, rmin, rmax, final_bgr = self.bgr_calc_rings(psf_ima_cut, par_cut)
+        print(rmin, rmax)
         psf_ima_no_bgr = psf_ima_cut - final_bgr
         psf_diffraction_limited = self.create_psf_diffraction_limited(xx, yy)
 
@@ -70,6 +77,16 @@ class SR_Calculator():
         norm_psf_ima = psf_ima_cut/np.sum(psf_ima_cut)
         return norm_psf_ima
 
+    def seq_sr(self, psf_ima):
+        srList = []
+        pix = np.arange(239)+15
+        prova = np.arange(79)+15
+        for i in prova:
+            print(i)
+            sr, n_bgr,n_dl = self.principal_main_cut(psf_ima, i)
+            srList.append(sr)
+        return np.array(srList), pix
+###
     def _proveBkg(self, psf_ima):
         bkg = background.MeanBackground()
         bkg_value = bkg.calc_background(psf_ima)
@@ -87,6 +104,7 @@ class SR_Calculator():
         norm3 = psf_ima_no_bgr/np.sum(psf_ima_no_bgr)
 
         return norm1, norm2, norm3
+###
 
     def fit_2dGaussianForPsfImaCut(self, psf_ima_cut, y_min, x_min):
         '''
@@ -123,7 +141,7 @@ class SR_Calculator():
         par_psf_ima_no_bgr[3] = new_par[3] + y_min #140
         return par_psf_ima_no_bgr
 
-    def bgr_calc(self, psf_ima, par, const_for_rmin=6):
+    def bgr_calc_rings(self, psf_ima, par, const_for_rmin=6):
         '''
         arg:
             psf_ima = camera frame containing the PSF
@@ -148,7 +166,7 @@ class SR_Calculator():
 
         cc = np.array([10,10,10,10])
         rmax = np.min((par[2], par[3], size[0]-par[2]-1,
-                       size[1]-par[3]-1)-cc).astype(int)
+                       size[1]-par[3]-1)).astype(int)   #-cc
         rmin = np.ceil(const_for_rmin * sigma)
         dr = 3
         #rmax = ((rmax-rmin)/dr)*dr+rmin
@@ -242,17 +260,3 @@ class SR_Calculator():
         en = np.sum(ima_no_bgr)
         return en
     ###
-
-    ### prova
-    # xc = 314; yc = 150
-    # pp = rr[yc-sr._pixelCut:yc+sr._pixelCut+1, xc-sr._pixelCut:xc+sr._pixelCut+1]
-    #
-            #
-#         y_min = (par_psf_ima[3]-rmax).astype(int)
-#         y_max = (par_psf_ima[3]+rmax).astype(int)
-#         x_min = (par_psf_ima[2]-rmax).astype(int)
-#         x_max = (par_psf_ima[2]+rmax).astype(int)
-#         psf_ima_rmax = psf_ima[y_min:y_max, x_min:x_max]
-#         xx_rmax = xx[y_min:y_max, x_min:x_max]
-#         yy_rmax = yy[y_min:y_max, x_min:x_max]
-        #
